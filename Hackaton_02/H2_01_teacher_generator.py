c9459e718dc6a06cf49d66ec14d3c6b71a80fab7
MacciEKyleee <map.cieszynski@gmail.com>
2022-04-02 17:55:10 +0200

    Hackaton_02

--- Hackaton_02/H2_01_teacher_generator.py
+++ Hackaton_02/H2_01_teacher_generator.py
@@ -6,6 +6,12 @@ def students_data():
 def clean_column_value(value):
     return value.strip()
 
+def save_txt(value):
+    with open('message.txt', 'a+') as f:
+        f.write(value)
+        f.write('*****************************\n\n')
+
+
 def main():
     students_class = []
     students_name = []
@@ -21,36 +27,33 @@ def main():
         students_missing_task.append(clean_column_value(splitted_line[3]))
         students_rating.append(clean_column_value(splitted_line[4]))
 
-    for element in students_missing_task:
-        try:
-            students_missing_task[element] == 'nieobecność'
-        except ValueError:
-            students_missing_task.insert(element,'0')
-
-
     message_for_students = []
 
     counter = len(students_rating)
 
     for i in range(1,counter):
+
+        # jak rzuci błędem - tj. user był nieobecny
+        # to nie będzie do niego maila
+        # a wrzucimy ich na inny plik, tych co nie chodzili
+        try:
+            if not students_rating[i].isdigit():
+                raise ValueError('Incorrect rating')
+        except:
+            with open('blacklist.txt', 'a+') as f:
+                f.write(f'{students_name[i]} {students_surname[i]} --> Nieobecność na zajęciach. \nProsimy zgłosić się do wykładowcy.\n')
+                f.write('\n')
+
         if (students_rating[i] != '5'):
+
             message = f"Hi {students_name[i]} {students_surname[i]},\n\nThis is a reminder that you have ({students_missing_task[i]}) tasks left to submit before you can graduate. \nYou're current grade is {students_rating[i]} and can increase to 5 if you submit all assignments before the due date.\n\n"
 
-            print(message)
-
-            with open('message.txt','a+') as f:
-                f.write(message)
-                f.write('*****************************\n\n')
-
+            save_txt(message)
 
         else:
             message = f"Hi {students_name[i]} {students_surname[i]},\n\nThis is a reminder that you have ({students_missing_task[i]}) tasks left to submit before you can graduate. \nYou're current grade is {students_rating[i]} - CONGRATULATIONS ! ! !.\n\n"
 
-            print(message)
-
-            with open('message.txt','a+') as f:
-                f.write(message)
-                f.write('------------------------------\n\n')
+            save_txt(message)
 
 if __name__ == '__main__':
     main()
