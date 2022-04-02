6b5534c5370d8879600cbb055a5b80bb1293f46a
MacciEKyleee <map.cieszynski@gmail.com>
2022-04-03 01:25:54 +0200

    Hackaton_02

--- Hackaton_02/H2_01_teacher_generator.py
+++ Hackaton_02/H2_01_teacher_generator.py
@@ -1,18 +1,23 @@
 def students_data():
+    # Function that retrieves data from a .csv file (data downloaded from excel)
     with open('Students.csv') as f:
         students_data = f.readlines()
     return students_data
 
 def clean_column_value(value):
+    # Function that clears whitespace for us
     return value.strip()
 
 def save_txt(value):
+    # Function that will save finished messages to the message.txt file
     with open('message.txt', 'a+') as f:
         f.write(value)
         f.write('*****************************\n\n')
 
 
 def main():
+    # we create lists with in turn: class, surnames and first names of students, grades which they do not have and the average of the obtained grades.
+
     students_class = []
     students_name = []
     students_surname = []
@@ -20,6 +25,8 @@ def main():
     students_rating = []
 
     for line in students_data():
+        # We fill the created lists with data taken from the .csv file
+
         splitted_line = line.split(',')
         students_class.append(clean_column_value(splitted_line[0]))
         students_name.append(clean_column_value(splitted_line[1]))
@@ -31,11 +38,13 @@ def main():
 
     counter = len(students_rating)
 
+    # We must offer students the opportunity to improve their grades if they return any missing papers
+    # Students who have submitted all their work receive the highest grade - they do not have to correct / report anything anymore
     for i in range(1,counter):
 
-        # jak rzuci błędem - tj. user był nieobecny
-        # to nie będzie do niego maila
-        # a wrzucimy ich na inny plik, tych co nie chodzili
+        # if the student was absent, it will display an error
+        # if he reports an error - the e-mail with the grade will not be sent to the student
+        # we will create a file called 'blacklist' where students with absences will be collected
         try:
             if not students_rating[i].isdigit():
                 raise ValueError('Incorrect rating')
